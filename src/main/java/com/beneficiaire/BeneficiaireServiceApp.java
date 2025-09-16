0a14b6e7af674a9b06bffd5733fbc71c05b245c2
fekihtomy <fekih.tomy@gmail.com>
2025-09-16 11:17:59 +0200

    Update BeneficiaireServiceApp.java

--- src/main/java/com/beneficiaire/BeneficiaireServiceApp.java
+++ src/main/java/com/beneficiaire/BeneficiaireServiceApp.java
@@ -1,1 +1,11 @@
+package com.beneficiaire;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiaireServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BeneficiaireServiceApp.class, args);
+    }
+}
