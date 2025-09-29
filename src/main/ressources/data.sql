8399262c0f16b934d65c9d456919acc5ab8d2f2c
fekihtomy <fekih.tomy@gmail.com>
2025-09-29 21:32:37 +0200

    Update data.sql

--- src/main/ressources/data.sql
+++ src/main/ressources/data.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO entreprises (nom, siren, adresse) VALUES
+('AlphaTech', '123456789', '10 rue de la République, Paris'),
+('BetaCorp', '987654321', '20 avenue des Champs, Lyon'),
+('GammaIndustries', '112233445', '30 boulevard Haussmann, Marseille');
