8db86ce0da18240187a3b0eb97990a395c508de2
fekihtomy <fekih.tomy@gmail.com>
2025-09-29 21:13:39 +0200

    Update EntrepriseRepository.java

--- src/main/java/com/beneficiaire/repository/EntrepriseRepository.java
+++ src/main/java/com/beneficiaire/repository/EntrepriseRepository.java
@@ -1,1 +1,15 @@
+package com.beneficiaire.repository;
 
+import com.beneficiaire.entity.Entreprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EntrepriseRepository extends JpaRepository<Entreprise, Long> {
+    
+    Optional<Entreprise> findBySiren(String siren);
+
+    boolean existsBySiren(String siren);
+}
