1af91ac48ba3b9f1db5111537fcbf4c4f221c8c7
tomy fekih <fekih.tomy@gmail.com>
2025-10-03 09:10:52 +0200

    Version améliorée du projet

---
+++ src/test/java/com/beneficiaire/RepobpifrancebeneficiaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beneficiaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepobpifrancebeneficiaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // test d'intégration basique : contexte Spring démarre
+    }
+}
